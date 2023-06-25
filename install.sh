e7658e299890effc503189bf902b7ec929fae90d
roopekj <40203658+roopekj@users.noreply.github.com>
2023-06-25 18:48:45 +0300

    Add time savers

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-# Add bash config to system .bashrc
-cat config/bash/bashrc >> ~/.bashrc
+# Add reference of repo's bash config to system .bashrc if not already present
+grep -qxF 'source $HOME/dotfiles/config/bash/bashrc' ~/.bashrc || echo 'source $HOME/dotfiles/config/bash/bashrc' >> ~/.bashrc
 
 # Use tmux.conf from this repo
 echo "source-file $HOME/dotfiles/config/tmux/tmux.conf" > ~/.tmux.conf
