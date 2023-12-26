dc0f59c96c503fb29b5c79cea13de604924c6452
roopekj <40203658+roopekj@users.noreply.github.com>
2023-12-26 20:32:42 +0200

    Update i3wm version, Add styling, Modify dmenu keybind

--- install.sh
+++ install.sh
@@ -15,7 +15,14 @@ wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsM
 unzip JetBrainsMono.zip && rm JetBrainsMono.zip
 sudo apt install breeze-icon-theme	# This is for krusader
 
-# Software
+# i3wm
+/usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2023.02.18_all.deb keyring.deb SHA256:a511ac5f10cd811f8a4ca44d665f2fa1add7a9f09bef238cdfad8461f5239cc4
+sudo apt install ./keyring.deb
+echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" | sudo tee /etc/apt/sources.list.d/sur5r-i3.list
+rm keyring.deb
+
+# Install packages
+sudo apt update
 sudo apt install neovim
 sudo snap install --classic code	# Cry about it
 sudo apt install kitty
