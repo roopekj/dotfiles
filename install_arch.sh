27d8d16fef6ba02b929f6c5cc31a4501372a1e9e
roopekj <40203658+roopekj@users.noreply.github.com>
2024-11-06 23:36:09 +0200

    Add Xorg config to disable mouse acceleration, Add miscellaneous packages

--- install_arch.sh
+++ install_arch.sh
@@ -76,5 +76,8 @@ sudo pacman -S xss-lock --noconfirm
 sudo usermod -aG users "$USER"
 sudo cp $HOME/dotfiles/scripts/suspend.rules /etc/polkit-1/rules.d
 
+# Add Xorg configuration (mouse acceleration)
+sudo cp $HOME/dotfiles/config/xorg/* /etc/X11/xorg.conf.d/
+
 # Misc
-sudo pacman -S tig extra/xorg-xrandr man-pages man-db network-manager-applet sshuttle maim xdotool ripgrep npm eza ncdu yazi --noconfirm
+sudo pacman -S tig extra/xorg-xrandr man-pages man-db network-manager-applet sshuttle maim xdotool ripgrep npm eza ncdu yazi chromium gimp vlc telegram-desktop discord --noconfirm
