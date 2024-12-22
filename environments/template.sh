5235690488d4e13069f001e8783b4bf59183c84b
roopekj <40203658+roopekj@users.noreply.github.com>
2024-12-22 11:50:00 +0200

    Modify monitor configuration

---
+++ environments/template.sh
@@ -0,0 +1,10 @@
+# NOTE: Template for a correctly formatted environment file, rename to `current.sh` to enable.
+
+# Monitor in order of their placement from left to right separated by spaces; `xrandr` to get identifiers, eg. HDMI-1.
+MONITORS="MONITOR_1 MONITOR_2"
+
+# Monitor resolutions in the same order as above and separated by spaces, eg. 1920x1080.
+RESOLUTIONS="RESOLUTION_1 RESOLUTION_2"
+
+# Identifier of the primary monitor.
+PRIMARY_MONITOR="MONITOR_2"
--- scripts/switch_monitors.sh
+++ scripts/switch_monitors.sh
@@ -1,4 +1,23 @@
 #!/bin/sh
+source $HOME/dotfiles/environments/current.sh
+MONITORS=($MONITORS)
+RESOLUTIONS=($RESOLUTIONS)
 
-xrandr --output HDMI-0 --mode 1920x1080 --left-of DP-2
-xrandr --output DP-2 --mode 1920x1080 --primary
+current_index=0
+for MONITOR in ${MONITORS[@]}
+do
+	# Set resolution
+	xrandr --output "$MONITOR" --mode "${RESOLUTIONS[$current_index]}"
+
+	# Set placement
+	if (( $current_index > 0 )); then
+		xrandr --output "$MONITOR" --right-of "${MONITORS[(( $current_index - 1 ))]}"
+	fi
+
+	# Set primary
+	if [ "$MONITOR" = "$PRIMARY_MONITOR" ]; then
+		xrandr --output "$MONITOR" --primary
+	fi
+
+	(( current_index++ ))
+done
